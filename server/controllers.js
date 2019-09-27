1f2f4817e93e46a75dbe071895c4c9996531e2d3
Jeremy Purser <jeremypurser@gmail.com>
2019-09-27 18:03:44 -0400

    Connect to redis, use cache on findNearbyRentals

--- server/controllers.js
+++ server/controllers.js
@@ -1,12 +1,15 @@
 const Pool = require('pg').Pool;
-const { user, password } = require('../config.js');
+const redis = require('redis');
+const { password } = require('../config.js');
+const connectionString = `postgres://postgres:${password}@ec2-18-222-27-79.us-east-2.compute.amazonaws.com:5432/housemania`;
 
 const pool = new Pool({
-  user,
-  host: 'localhost',
-  database: 'housemania',
-  password,
-  port: 5432
+  connectionString
+});
+
+const client = redis.createClient({
+  port: 6379,
+  host: 'ec2-3-19-237-85.us-east-2.compute.amazonaws.com'
 });
 
 exports.createRental = (req, res) => {
@@ -17,25 +20,37 @@ exports.createRental = (req, res) => {
     reviewcount, zip) VALUES ($1, $2, $3, $4, $5, 0, 0, $6)';
   pool.query(query, [imgurl, location, type, title, cost, zip])
     .then(result => {
-      res.status(201).json(result);
+      res.status(201).end();
     })
     .catch(e => {
+      console.log(e);
       res.status(500);
     });
 };
 
 exports.findNearbyRentals = (req, res) => {
-  const query = 'SELECT * FROM rentals WHERE zip > $1 AND zip < $2 \
-    ORDER by id DESC FETCH FIRST 12 ROW ONLY';
-  const searchZip = Number(req.params.zip);
-  const params = [(searchZip - 500), (searchZip + 500)];
-  pool.query(query, params)
-    .then(result => {
-      res.status(200).json(result.rows);
-    })
-    .catch(e => {
-      res.status(500);
-    });
+  client.get(req.params.zip, (err, records) => {
+    if (err) {
+      throw err;
+    }
+    if (records) {
+      res.json(JSON.parse(records));
+    } else {
+
+      const query = 'SELECT * FROM rentals WHERE zip > $1 AND zip < $2 \
+        ORDER by id DESC FETCH FIRST 12 ROW ONLY';
+      const searchZip = Number(req.params.zip);
+      const params = [(searchZip - 500), (searchZip + 500)];
+      pool.query(query, params)
+        .then(result => {
+          client.setex(req.params.zip, 3600, JSON.stringify(result.rows));
+          res.status(200).json(result.rows);
+        })
+        .catch(e => {
+          res.status(500).end();
+        });
+    }
+  });
 };
 
 exports.updateRental = (req, res) => {
